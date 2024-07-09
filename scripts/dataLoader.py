d4cb954e4865d4292daac4b0acb0440fedce20ea
jake-joseph <jakejos@umich.edu>
2024-07-09 17:15:34 -0400

    added doc strings

--- scripts/dataLoader.py
+++ scripts/dataLoader.py
@@ -9,71 +9,108 @@ import cv2
 from utils import calculate_angle
 
 class DataLoader:
-        def __init__(self, name, file, threshold, window,type='DLC'):
-            self.file = file
-            self.name = name
-            self.threshold = threshold
-            self.window = window
-            self.type = type
-            self.wrist, self.index = self.loadData()
+    """
+    This class loads in NHP predicted joint angles and extracts angles and does preprocessing.
 
-        def loadData(self):
+    Attributes:
+        name (str): The name of the DataLoader instance.
+        file (str): The file path of the data file to load.
+        threshold (float): The likelihood threshold for linear interpolation.
+        window (int): The window size for moving average.
+        type (str, optional): The type of data. Defaults to 'DLC'.
+        wrist (numpy.ndarray): The wrist angles.
+        index (numpy.ndarray): The index angles.
+        x (list): The x coordinates of the points.
+        y (list): The y coordinates of the points.
+    """
 
-            data = pd.read_csv(self.file, skiprows=3, header=None)
+    def __init__(self, name, file, threshold, window, type='DLC'):
+        self.file = file
+        self.name = name
+        self.threshold = threshold
+        self.window = window
+        self.type = type
+        self.wrist, self.index = self.loadData()
 
-            def interpolate(df, x, y, likelihood, threshold):
-                for i in range(1, len(df)):
-                    if df.iloc[i, likelihood] < threshold:
-                        df.iloc[i, x] = df.iloc[i-1, x]
-                        df.iloc[i, y] = df.iloc[i-1, y]
+    def loadData(self):
+        """
+        Load the data from the file, perform linear interpolation and moving average,
+        and extract wrist and index angles.
 
-            # Linear Interpolation based on likelihood threshold
+        Returns:
+            tuple: A tuple containing the wrist angles and index angles as numpy arrays.
+        """
+        data = pd.read_csv(self.file, skiprows=3, header=None)
 
+        def interpolate(df, x, y, likelihood, threshold):
+            for i in range(1, len(df)):
+                if df.iloc[i, likelihood] < threshold:
+                    df.iloc[i, x] = df.iloc[i-1, x]
+                    df.iloc[i, y] = df.iloc[i-1, y]
+
+        # Linear Interpolation based on likelihood threshold
+        if self.type == 'DLC':
+            step = 1
+        else:
+            step = 3  # NOTE: This is for sleap since the first 3 columns are not coordinates
+
+        for i in range(step, len(data.columns), 3):
+            likelihood = i + 2
+            x_col = i
+            y_col = i + 1
+            interpolate(data, x_col, y_col, likelihood, self.threshold)
+
+        # Moving average
+        for i in range(step, len(data.columns), 3):
+            x = data.columns[i]
+            y = data.columns[i+1]
+            data[x] = data[x].rolling(window=self.window, min_periods=1).mean()
+            data[y] = data[y].rolling(window=self.window, min_periods=1).mean()
+
+        wristAngle = []
+        indexAngle = []
+        self.x = []
+        self.y = []
+        for i in range(len(data)):  # save angle for each posture for all frames
             if self.type == 'DLC':
-                step =1
+                forearm = (data.iloc[i][data.columns[13]], data.iloc[i][data.columns[14]])
+                wrist = (data.iloc[i][data.columns[10]], data.iloc[i][data.columns[11]])
+                mcp = (data.iloc[i][data.columns[7]], data.iloc[i][data.columns[8]])
             else:
-                step = 3  # NOTE: This is for sleap since the first 3 columns are not coordinates
-
-            for i in range(step,len(data.columns),3):
-                likelihood = i + 2  
-                x_col = i  
-                y_col = i + 1  
-                interpolate(data, x_col, y_col, likelihood, self.threshold)
-
-            # Moving average
-            for i in range(step, len(data.columns),3):
-                x = data.columns[i]
-                y = data.columns[i+1]
-                data[x] = data[x].rolling(window=self.window, min_periods=1).mean()
-                data[y] = data[y].rolling(window=self.window, min_periods=1).mean()        
-
-            wristAngle = []
-            indexAngle = []
-            self.x = []
-            self.y = []
-            for i in range(len(data)):  #save angle for each posture for all frames
-                if self.type == 'DLC':
-                    forearm = (data.iloc[i][data.columns[13]], data.iloc[i][data.columns[14]])
-                    wrist = (data.iloc[i][data.columns[10]], data.iloc[i][data.columns[11]])
-                    mcp = (data.iloc[i][data.columns[7]], data.iloc[i][data.columns[8]])
-                else:
-                    forearm = (data.iloc[i][data.columns[15]], data.iloc[i][data.columns[16]])
-                    wrist = (data.iloc[i][data.columns[12]], data.iloc[i][data.columns[13]])
-                    mcp = (data.iloc[i][data.columns[9]], data.iloc[i][data.columns[10]])
-                    pip = (data.iloc[i][data.columns[6]], data.iloc[i][data.columns[7]])
-                    dip = (data.iloc[i][data.columns[3]], data.iloc[i][data.columns[4]])
-                # Unpack the points into x and y coordinates
-                self.x.append([forearm[0], wrist[0], mcp[0], pip[0], dip[0]])
-                self.y.append([forearm[1], wrist[1], mcp[1], pip[1], dip[1]])
-                wristAngle.append(calculate_angle(forearm, wrist, mcp))
-                indexAngle.append(calculate_angle(wrist, mcp, pip)) # NOTE: This is using PIP as a vertex, some might use DIP
-            return np.array(wristAngle), np.array(indexAngle)
+                forearm = (data.iloc[i][data.columns[15]], data.iloc[i][data.columns[16]])
+                wrist = (data.iloc[i][data.columns[12]], data.iloc[i][data.columns[13]])
+                mcp = (data.iloc[i][data.columns[9]], data.iloc[i][data.columns[10]])
+                pip = (data.iloc[i][data.columns[6]], data.iloc[i][data.columns[7]])
+                dip = (data.iloc[i][data.columns[3]], data.iloc[i][data.columns[4]])
+            # Unpack the points into x and y coordinates
+            self.x.append([forearm[0], wrist[0], mcp[0], pip[0], dip[0]])
+            self.y.append([forearm[1], wrist[1], mcp[1], pip[1], dip[1]])
+            wristAngle.append(calculate_angle(forearm, wrist, mcp))
+            indexAngle.append(calculate_angle(wrist, mcp, pip))  # NOTE: This is using PIP as a vertex, some might use DIP
+        return np.array(wristAngle), np.array(indexAngle)
 
 
 class FatigueAnalysis(DataLoader):
-    def __init__(self, name, file, threshold, window, type='DLC',peakWindow=20,height=130, prominence=10, width=10, distance=10):
+    """
+    This class represents a specific analysis for the dmpFES paper.
+    It inherits from the DataLoader class and provides methods for fatigue analysis.
+
+    Args:
+        name (str): The name of the analysis.
+        file (str): The file path of the data file.
+        threshold (float): The threshold value for peak detection.
+        window (int): The window size for peak detection.
+        type (str, optional): The type of data. Defaults to 'DLC'.
+        peakWindow (int, optional): The window size for peak analysis. Defaults to 20.
+        height (int, optional): The height threshold for peak detection. Defaults to 130.
+        prominence (int, optional): The prominence threshold for peak detection. Defaults to 10.
+        width (int, optional): The width threshold for peak detection. Defaults to 10.
+        distance (int, optional): The distance threshold for peak detection. Defaults to 10.
+    """
+
+    def __init__(self, name, file, threshold, window, type='DLC', peakWindow=20, height=130, prominence=10, width=10, distance=10):
         super().__init__(name, file, threshold, window, type)
-        self.data = self.wrist  #Only using wrist angle for this paper
+        self.data = self.wrist  # Only using wrist angle for this paper
         self.peakWindow = peakWindow
         self.height = height
         self.prominence = prominence
@@ -82,80 +119,107 @@ class FatigueAnalysis(DataLoader):
         self.getStimAngle()
 
     def getStimAngle(self):
-        self.peaks, _ = find_peaks(self.data, height=self.height, prominence=self.prominence, width=self.width, distance=self.distance)  
+        """
+        This method calculates the stimulus angle for the analysis.
+        It detects peaks in the data and filters out any NaN values.
+        It also plots the data to visually inspect the detected peaks.
+        """
+        self.peaks, _ = find_peaks(self.data, height=self.height, prominence=self.prominence, width=self.width, distance=self.distance)
 
         angleChanges = []
         for i in range(len(self.peaks)):
-            max = np.max(self.data[self.peaks[i]-self.peakWindow:self.peaks[i]+self.peakWindow])
-            angleChanges.append(max) # - self.data[self.valleys[-restIdx]])
-        self.angleChanges = angleChanges
+            max = np.max(self.data[self.peaks[i] - self.peakWindow:self.peaks[i] + self.peakWindow])
+            angleChanges.append(max)
+        angleChanges = angleChanges
+        means = np.array(angleChanges)
+        mask = ~np.isnan(means)
+        self.filtered_means = means[mask]
+        self.filtered_indices = np.arange(len(means))[mask]
 
         # Plot to visually inspect if peaks were correctly detected
         plt.figure(figsize=(25, 5))
         plt.plot(self.data)
-        plt.plot(self.peaks, self.angleChanges, ".r", label='Peaks')
+        plt.plot(self.peaks, self.filtered_means, ".r", label='Peaks')
         plt.title(self.name)
         plt.xlabel("Frames")
         plt.ylabel("Absolute Angle (Degrees)")
         plt.show()
 
     def stimProfiles(self, stimuli, lengths, starts):
+        """
+        This method plots the stimulus profiles for the given stimuli, lengths, and start frames.
 
+        Args:
+            stimuli (list): List of stimulus indices.
+            lengths (list): List of stimulus lengths.
+            starts (list): List of start frames for each stimulus.
+        """
         fig, axes = plt.subplots(1, length(stimuli), figsize=(20, 5), sharex=True, sharey=True)
         axes = axes.flatten()
 
-        def plotSingleStim(stimNum,ax, length, start):
+        def plotSingleStim(stimNum, ax, length, start):
+            """
+            Helper function to plot a single stimulus.
+
+            Args:
+                stimNum (int): The index of the stimulus.
+                ax (matplotlib.axes.Axes): The axes object to plot on.
+                length (int): The length of the stimulus.
+                start (int): The start frame of the stimulus.
+            """
             stim = self.peaks[stimNum]
-            subset = self.data[stim-start:stim+length]
+            subset = self.data[stim - start:stim + length]
             ax.plot(subset, color='black')
-            ax.set_title(f"Stim {stimNum+1}")
+            ax.set_title(f"Stim {self.filtered_indices[stimNum] + 1}")
             ax.set_xlabel("Frame")
             ax.set_ylabel("Absolute Angle (degrees)")
 
-
         for i, (stim, length, start) in enumerate(zip(stimuli, lengths, starts)):
-            plotSingleStim(stim, axes[i], length,start)
-            axes[i].set_ylim(110, 200)  
+            plotSingleStim(stim, axes[i], length, start)
+            axes[i].set_ylim(110, 200)
             axes[i].axhline(y=180, color='red', linestyle='--')
             plt.suptitle('Napier')
             plt.ylabel('Absolute Angle (degrees)')
             plt.tight_layout()
             plt.show()
-  
 
-    def fitExponentialDecay(self, guess, removeStartSlice = 0):
+    def fitExponentialDecay(self, guess, removeStartSlice=0):
+        """
+        This method fits an exponential decay function to the filtered means.
 
-        # Fit exponential decay to means
+        Args:
+            guess (list): Initial guess values for the exponential decay parameters.
+            removeStartSlice (int, optional): Number of initial data points to remove. Defaults to 0.
+        """
         def exponential_decay(x, a, b, c):
             return a * np.exp(-b * x) + c
-        
-        def double_exponential_decay(x, a, b, c, d, e):
-            return a*np.exp(-b*x) + c*np.exp(-d*x) + e
-
-        # bounds = ([0, 0, 0, 0, 0], [np.inf, np.inf, np.inf, np.inf, np.inf])
-
-        # popt1, pcov = curve_fit(double_exponential_decay, self.filtered_indices[removeStartSlice:], self.filtered_means[removeStartSlice:], maxfev=10000,p0=[1,0,5,0,160], bounds=bounds)
-        # self.double_exponential = double_exponential_decay(self.filtered_indices, *popt1)
 
         bounds = ([0, 0, 0], [np.inf, np.inf, np.inf])
-        
+
         popt, _ = curve_fit(exponential_decay, self.filtered_indices[removeStartSlice:], self.filtered_means[removeStartSlice:], maxfev=10000, p0=guess, bounds=bounds)
         self.exponential = exponential_decay(self.filtered_indices[removeStartSlice:], *popt)
-        return popt
+        self.popt = popt
 
-    def makeVideo(self, video_file, saveName,stimNum, start, sliceLength):
+    def makeVideo(self, video_file, saveName, stimNum, start, sliceLength):
+        """
+        This method creates a video with annotations for a specific stimulus.
 
+        Args:
+            video_file (str): The file path of the video.
+            saveName (str): The file path to save the annotated video.
+            stimNum (int): The index of the stimulus.
+            start (int): The start frame of the stimulus.
+            sliceLength (int): The length of the stimulus slice.
+        """
         stim = self.peaks[stimNum]
         startFrame = stim - start
         endFrame = stim + sliceLength
-
-        # clipLength = n *30
 
         # Open the video
         cap = cv2.VideoCapture(video_file)
         if not cap.isOpened():
             print("Error opening video file")
-        
+
         cap.set(cv2.CAP_PROP_POS_FRAMES, startFrame)
 
         # Set up the writer object to write your video
@@ -177,56 +241,61 @@ class FatigueAnalysis(DataLoader):
 
                 frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 ax.imshow(frame_rgb)
-                # Small white dots with thick lines
                 ax.plot(self.x[i], self.y[i], 'o-', color='white', markersize=3, linewidth=5)
-                # Large white markers with thick black edges
-                ax.scatter(self.x[i], self.y[i], c='white', s=300, linewidth=3, edgecolors='black',zorder=10)
+                ax.scatter(self.x[i], self.y[i], c='white', s=300, linewidth=3, edgecolors='black', zorder=10)
 
-   
-        # clipLength = n *30
-             
-
-                # Fixed position for the angle text (in axis coordinates)
                 x_pos = 0.7  # 80% from the left of the figure
                 y_pos = 0.7  # 50% from the bottom of the figure
 
-                # Create a smaller rectangle patch
-                rect = patches.Rectangle((x_pos-0.08, y_pos-0.05), 0.16, 0.1,
-                                        facecolor='gray', alpha=0.5,
-                                        transform=ax.transAxes)
+                rect = patches.Rectangle((x_pos - 0.08, y_pos - 0.05), 0.16, 0.1,
+                                         facecolor='gray', alpha=0.5,
+                                         transform=ax.transAxes)
 
-                # Add the rectangle to the plot
                 ax.add_patch(rect)
 
-                # Add the text on top of the rectangle
                 ax.text(x_pos, y_pos,
                         f'Angle: {self.data[i]:.0f}',
                         color='white', fontsize=12,
                         ha='center', va='center',
                         transform=ax.transAxes)
 
-                fig.suptitle(f'Monkey {self.name[:1]} Stim {stimNum+1}', 
-                            ha='center', va='top', fontsize=20, weight='bold', color='white')
-                
-                # ax.text(0.5, 0.95, f'Max Angle: {self.angleChanges[stimNum]:.0f}', color='white', fontsize=16, ha='center', va='top', transform=ax.transAxes)
+                fig.suptitle(f'Monkey {self.name[:1]} Stim {stimNum + 1}',
+                             ha='center', va='top', fontsize=20, weight='bold', color='white')
+
                 ax.axis('off')
-                # plt.tight_layout()
-                # Write the current frame to the video
                 writer.grab_frame()
                 ax.clear()
 
                 i += 1
 
-        # Release the video capture object and close the figure
         cap.release()
         plt.close(fig)
 
+def plotDecay(data1, data2):
+    """
+    Plot the decay of ECRB fatigue for Monkey R and Monkey N.
+
+    Args:
+        data1 (object): Data object for Monkey R.
+        data2 (object): Data object for Monkey N.
+    """
+
+    plt.figure(figsize=(8, 6))
+    plt.scatter(data1.filtered_indices, data1.filtered_means, label=data1.name, color='#662506', s=20)
+
+    # Slicing this plot since we didn't include the first 6 values in the fit
+    plt.plot(data1.filtered_indices[6:], data1.exponential, label=r'Exponential: $\mathregular{%.0f \cdot e^{-%.2f x} + %.0f}$' % (data1.popt[0], data1.popt[1], round(data1.popt[2],-1)), color='#662506', linestyle='--')
+
+    plt.scatter(data2.filtered_indices, data2.angleChanges, label=data2.name, color='#3182bd', s=20)
+    plt.plot(data2.exponential, label=r'Exponential: $\mathregular{%d \cdot e^{-%.2f x} + %.0f}$' % (round(data2.popt[0]), data2.pop[1], round(data2.popt[2],-1)), color='#3182bd', linestyle='--')
+
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.xlabel("Stimulus Number", fontsize=14)
+    plt.ylabel("Absolute Angle (Degrees)", fontsize=14)
+    plt.legend(loc='lower right', fontsize=12)
+    plt.title("ECRB Fatigue Decay", fontsize=16)
+    plt.show()
+
 if __name__ == "__main__":
-    # Create an instance of FatigueAnalysis
-    fatigue_analysis = FatigueAnalysis("Monkey", "data.csv", 0.5, 5)
-    
-    # Call the necessary methods
-    fatigue_analysis.getAngleChange(10)
-    fatigue_analysis.plotAll()
-    fatigue_analysis.fitExponentialDecay([1, 0.1, 100])
-    fatigue_analysis.makeVideo("video.mp4", "output.mp4", 0, 2, 150)
+    pass
